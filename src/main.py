14d8dfe551850e4cf1bbff436f45fbca82e5a17f
Lkchau <32500267+Lkchau@users.noreply.github.com>
2019-12-15 20:47:26 -0500

    Added some tests and astor

--- src/main.py
+++ src/main.py
@@ -1,26 +1,95 @@
 import ast, sys, inspect
+from astor import astor
 from os import path
+# Task 1 - Find recursive functions
+# Task 2 - Find tail-end recurisive functions
+# Task 3 - Figure out how to convert that to an iterative version
+# Task 4 - Instrument it into the body of the tail-end recursive function
+# Task 5 - Output the new code into a new file or simply edit the existing file
+# Task 6 - Run to make sure that the result is as expected.
+
+tailRecursiveFunctions = []
+
+def hasTailRecursiveFunction(tree):
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef):
+            # print("Found function def at line " + str(node.lineno))
+            funcName = node.name
+            for i in node.body:
+                if isinstance(i, ast.Return):
+                    retVal = i.value
+                    if isinstance(retVal, ast.Call):
+                        retFuncName = retVal.func.id
+                        if(retFuncName == funcName):
+                            print("Tail-end Recursive Function at: " + str(node.lineno))
+                            tailRecursiveFunctions.append(retFuncName)
+                            return True
+    return False
+
+def instrument(tree):
+
+    statements = []
+    for statement in tree.body:
+        if isinstance(statement, ast.FunctionDef) and statement.name in tailRecursiveFunctions:
+            statement = instrument_body(statement)
+        statements = statement
+
+    tree.body = statements
+    code = astor.to_source(tree)
+    return code
+
+def instrument_body(function_def):
+    parameters = []
+    statements = []
+    print(function_def.name)
+    for arg in function_def.args.args:
+        parameters.append(arg.arg)
+    # statements.append('While True:')
+    for statement in function_def.body:
+        statements.append(statement)
+
+    function_def.body = statements
+    print(function_def.body)
+    return function_def            
+            # print(statement)
+    # else:
+    #     # statements += instrument_statement(statement, parameters)
+    #     for var in parameters:
+    #         if var.startswith("_n_"):
+    #             statements += instrument_statement(function_def.name,var)
+    # function_def.body = statements
+    # return function_def
+
+# def instrument_statement(name,parameter):
+#     if_statement = ast.parse('if ' + str(parameter) + ' is None:\n \traise Exception(\'' + str(parameter) + ' is given None in ' + name + '\')')
+#     return [if_statement]
+
 
 # Main function should read an input file in the same directory and find function defs
 if __name__ == "__main__":
     try:
         print("-----")
         # Assert that they will call python main <filename> or python main <filename>.py
+        currPath = path.abspath(__file__+'\..')
+        currPath = currPath +'\\test\\'
         assert len(sys.argv) == 2, "Invalid amount of arguments!"
-        assert path.exists(sys.argv[1]) or path.exists(sys.argv[1] + ".py" ), sys.argv[1] + " does not exist!"
-        if path.exists(sys.argv[1] + ".py"):
-            filename = sys.argv[1] + ".py"
+        assert path.exists(path.join(currPath, sys.argv[1])) or path.exists(path.join(currPath, sys.argv[1] + ".py" )), sys.argv[1] + " does not exist!"
+        if path.exists(path.join(currPath,sys.argv[1] + ".py")):
+            filename = path.join(currPath,sys.argv[1] + ".py")
         else:
-            filename = sys.argv[1]
+            filename = path.join(currPath,sys.argv[1])
         
         # Open and read the file as code!
         with open(filename, 'r') as code_file:
             code = code_file.read()
             # Create the ast
             tree = ast.parse(code)
+            print(ast.dump(tree))
+            assert(hasTailRecursiveFunction(tree)), "No Tail-End Recursion exists in " + sys.argv[1]
+            print(instrument(tree))
         
             # Call our function to find the functions.
-            
+
             print("-----")
     except AssertionError as e:
         print(e.args[0])
--- src/test/factorial.py
+++ src/test/factorial.py
@@ -6,6 +6,22 @@ def factorial(n, a = 1):
 
     return factorial(n - 1, n * a) 
 
+def alteredFactorial(n, a = 1):
+    while True:
+        if(n == 0):
+            return a
+        n , a = n-1,n*a
+        continue
+        break
+
+
+# Recursion but not tail-end
+def factorial2 (n):
+    if (n == 0):
+        return 1
+    else:
+        return n * factorial2(n - 1);
+
 # Iterative version?
 
 def factorialIter(n):
@@ -17,5 +33,7 @@ def factorialIter(n):
     return val
 
 # Print tests to make sure they are indeed printing the factorial
-print("Recursive factorial test: " + factorial(5))
-print("Iterative factorial test: " + factorialIter(5))
+print("tail-end factorial test: " + str(factorial(5)))
+print("Alt tail-end factorial test: " + str(alteredFactorial(5)))
+print("Recursive factorial test: " + str(factorial2(5)))
+print("Iterative factorial test: " + str(factorialIter(5)))
